5f3c89377a5717ee9659af47d6a2f2906c02bdbf
nithinbharathi <nithinbharathik@outlook.com>
2025-08-02 15:24:56 -0300

    added benchmark for measuring approx. space consumption to get a sense of memory used by primitive and wrapper class objects

--- jmatrix/Benchmark.java
+++ jmatrix/Benchmark.java
@@ -1,39 +1,46 @@
 package jmatrix;
-import java.util.Arrays;
 import java.util.Random;
 
-import jdk.incubator.vector.DoubleVector;
-
-import jdk.incubator.vector.VectorSpecies;
-
 public class Benchmark {
-	private static int r = 10000, c = 10000;
-    static final VectorSpecies<Double> SPECIES = DoubleVector.SPECIES_PREFERRED;
-
-
+	private static int r = 100, c = 100;
 	public static void main(String[] args) {
-		double time = 0;
-		for(int i =0;i<10;i++)
-			time += v3();
-		
-		System.out.println("Avg Time: "+(time/10));
-		
 
-		
-		
-		/*Double a[][] = new Double[r][c];
-		
-		for (int i = 0; i < r; i++) {
-		    for (int j = 0; j < c; j++) {
-		        a[i][j] = Double.valueOf(0.0);
-		    }
-		}*/
-		
+		int memoryChoice[][] = {{5000,5000}};
 
+		for(int times = 0;times<1;times++){
+			for(int i = 0;i<memoryChoice.length;i++){
+				int rowSize = memoryChoice[i][0];
+				int colSize = memoryChoice[i][0];
+				memory2(rowSize,colSize);
+				printRuntimeMemoryUsage();
+			}
+		}
 
-		
-		
 	}
+
+	private static void memory1(int rowSize, int colSize){
+		System.out.println("declaring a matrix using primitive double type....\n rowSize: "+rowSize+" colSize: "+colSize +"\n");
+		double matrix[][] = new double[rowSize][colSize];
+		Random random = new Random();
+		for(int i=0;i<rowSize;i++){
+			for(int j=0;j<colSize;j++){
+				matrix[i][j] = random.nextDouble();
+			}
+		}
+
+	}
+
+	private static void memory2(int rowSize, int colSize){
+		System.out.println("Declaring a matrix using wrapper Double type...\n rowSize: "+rowSize+" colSize: "+colSize +"\n");
+		Double matrix[][] = new Double[rowSize][colSize];
+		Random random = new Random();
+		for(int i = 0;i<rowSize;i++){
+			for(int j =0;j<colSize;j++){
+				matrix[i][j] = random.nextDouble();
+			}
+		}
+	}
+		
 	
 	private static Double[][] generateRandomArray(int r, int c) {
 		Double arr[][] = new Double[r][c];
@@ -112,43 +119,6 @@ public class Benchmark {
 		
 		return time;
 		
-	}
-	
-	private static double v3() {
-		double[] a = new double[1024];
-		double[] b = new double[1024];
-		double[] result = new double[1024];
-
-        // Initialize
-        for (int i = 0; i < a.length; i++) {
-            a[i] = i;
-            b[i] = 2 * i;
-        }
-
-        int i = 0;
-        int length = a.length;
-        
-        long startTime = System.nanoTime();
-
-        // SIMD addition
-        for (; i < SPECIES.loopBound(length); i += SPECIES.length()) {
-            // Load vectors
-            var va = DoubleVector.fromArray(SPECIES, a, i);
-            var vb = DoubleVector.fromArray(SPECIES, b, i);
-            var vc = va.add(vb);
-            vc.intoArray(result, i);
-        }
-
-        // Handle tail elements (not SIMD-compatible)
-        for (; i < length; i++) {
-            result[i] = a[i] + b[i];
-        }
-        
-        long endTime = System.nanoTime();
-
-        System.out.println("Result[0] = " + result[0]);
-        
-        return (endTime - startTime)/1e9;
 	}
 
 }
